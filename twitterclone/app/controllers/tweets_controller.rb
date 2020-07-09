d405bfbf5c654c93a69aeb327ede4e51d8c3b2f2
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-07-09 16:33:43 +0900

    一覧ページの作成,コントローラへアクションの記入

--- twitterclone/app/controllers/tweets_controller.rb
+++ twitterclone/app/controllers/tweets_controller.rb
@@ -1,8 +1,38 @@
 class TweetsController < ApplicationController
+  def index
+    @tweet = Tweet.all
+  end
+  def show
+  end
   def new
     @tweet = Tweet.new
   end
-  def confirm
+  def create
+    Tweet.create(blog_params)
+    if @blog.save
+    redirect_to new_tweet_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
     
   end
+  def confirm
+   @tweet = Tweet.new(blog_params)
+   render :new if @tweet.inbalid?
+  end
+
+  private
+  def blog_params
+    params.require(:tweet).permit(:content)
+  end
+
+
 end
